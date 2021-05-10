6b3fb251cbba92f48c728fc282ec20973b2347e0
Ollie <oliverdanielsmith@hotmail.com>
2021-05-10 11:14:44 +0100

    Added test for formatting with plural visits

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -6,5 +6,11 @@ describe List do
       list = List.new(logs)
       expect { list.print_visits }.to output("/help_page/1 1 visit \n").to_stdout
     end
+
+    it 'formats the logs into human readable format and then prints to the terminal. There are two logs to the same page' do
+      logs = {"/help_page/1"=>["126.318.035.038"], "/help_page/1"=>["929.398.951.889"]}
+      list = List.new(logs)
+      expect { list.print_visits }.to output("/help_page/1 2 visits \n").to_stdout
+    end
   end
 end
