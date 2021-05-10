c2683eb9d7bf3c1d3e4c402b4422e5cf05f6bcbf
Ollie <oliverdanielsmith@hotmail.com>
2021-05-10 11:02:24 +0100

    Passed test of creating a unique list

--- lib/parser.rb
+++ lib/parser.rb
@@ -9,26 +9,34 @@ class Parser
     @logs = Hash.new
   end
 
-  def create_list
-    read_logs
+  def create_list(is_unique = false)
+    read_logs(is_unique)
     @list = @list_class.new(@logs)
   end
 
   private
 
-  def read_logs
+  def read_logs(is_unique)
     File.foreach(@file) do |line| 
       split_line = line.split(" ")
       page_name, ip_address = split_line[0], split_line[1]  
-      add_log(page_name, ip_address)
+      add_log(page_name, ip_address, is_unique)
     end
   end
 
-  def add_log(page_name, ip_address)
-    if @logs[page_name]
-      @logs[page_name] << ip_address
-    else
-      @logs[page_name] = [ip_address]
+  def add_log(page_name, ip_address, is_unique)
+    if is_unique
+      if @logs[page_name]
+        @logs[page_name] << ip_address unless @logs[page_name].include?(ip_address)
+      else
+        @logs[page_name] = [ip_address]
+      end
+    else    
+      if @logs[page_name]
+        @logs[page_name] << ip_address
+      else
+        @logs[page_name] = [ip_address]
+      end
     end
   end
 end
