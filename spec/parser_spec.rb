a72ec5def03547cbb2eb72c21071e689b981b720
Ollie <oliverdanielsmith@hotmail.com>
2021-05-10 10:56:33 +0100

    Added test for creating a unique list

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -12,7 +12,7 @@ describe Parser do
       expect(parser.list).to eq(list_double)
     end
 
-    it 'creates a list and the logs stored on that list are a hash containing the onformation in the file. The file has one row.' do
+    it 'creates a list and the logs stored on that list are a hash containing the information in the file. The file has one row.' do
       dir = File.dirname(__FILE__)
       file = File.open(File.join(dir, 'test_logs_1.log'))
       parser = Parser.new(file)
@@ -20,7 +20,7 @@ describe Parser do
       expect(parser.list.logs).to eq({"/help_page/1"=>["126.318.035.038"]})
     end
 
-    it 'creates a list and the logs stored on that list are a hash containing the onformation in the file. The file has two visits to the same page.' do
+    it 'creates a list and the logs stored on that list are a hash containing the information in the file. The file has two visits to the same page.' do
       dir = File.dirname(__FILE__)
       file = File.open(File.join(dir, 'test_logs_2.log'))
       parser = Parser.new(file)
@@ -28,12 +28,20 @@ describe Parser do
       expect(parser.list.logs).to eq({"/help_page/1"=>["126.318.035.038", "929.398.951.889"]})
     end
 
-    it 'creates a list and the logs stored on that list are a hash containing the onformation in the file. The file has two visits to the same page from the same ip_address.' do
+    it 'creates a list and the logs stored on that list are a hash containing the information in the file. The file has two visits to the same page from the same ip_address.' do
       dir = File.dirname(__FILE__)
       file = File.open(File.join(dir, 'test_logs_3.log'))
       parser = Parser.new(file)
       parser.create_list
       expect(parser.list.logs).to eq({"/help_page/1"=>["126.318.035.038", "126.318.035.038"]})
+    end
+
+    it 'creates a unique list and the logs stored on that list are a hash containing the information in the file. The file has one row.' do
+      dir = File.dirname(__FILE__)
+      file = File.open(File.join(dir, 'test_logs_3.log'))
+      parser = Parser.new(file)
+      parser.create_list(is_unique = true)
+      expect(parser.list.logs).to eq({"/help_page/1"=>["126.318.035.038"]})
     end
   end
 end
