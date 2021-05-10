b58423d4c6f72792e31753d3a083ba5721670608
Ollie <oliverdanielsmith@hotmail.com>
2021-05-10 11:12:54 +0100

    Passed test for formatting and printing visits

--- lib/list.rb
+++ lib/list.rb
@@ -1,6 +1,15 @@
 class List
   attr_reader :logs
+
   def initialize(logs)
     @logs = logs
+    @visits = []
+  end
+
+  def print_visits
+    @logs.each do |page_name, ip_addresses| 
+      @visits << "#{page_name} #{ip_addresses.length} visit "
+    end
+    puts @visits.join
   end
 end
