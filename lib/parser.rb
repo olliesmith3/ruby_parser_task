8cf9392c164d42e7efcfc05c07b02600eeecf325
Ollie <oliverdanielsmith@hotmail.com>
2021-05-10 10:38:44 +0100

    Refactored to separate responsibility of reading the file to a private method as this is not used by the user

--- lib/parser.rb
+++ lib/parser.rb
@@ -10,11 +10,17 @@ class Parser
   end
 
   def create_list
+    read_logs
+    @list = @list_class.new(@logs)
+  end
+
+  private
+
+  def read_logs
     File.foreach(@file) do |line| 
       split_line = line.split(" ")
       page_name, ip_address = split_line[0], split_line[1]  
       @logs[page_name] = [ip_address]
     end
-    @list = @list_class.new(@logs)
   end
 end
